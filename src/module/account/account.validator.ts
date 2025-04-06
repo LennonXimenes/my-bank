990f4384c6e839329f2de8b7e5a39476c151e70e
Lennon_Ximenes <lennon_ximenes@hotmail.com>
2025-04-06 17:27:39 -0300

    add verify exists in validator of account

--- src/module/account/account.validator.ts
+++ src/module/account/account.validator.ts
@@ -1,7 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { AccountRepository } from "./account.repository";
 
 @Injectable()
 export class AccountValidator {
 	constructor(private readonly repository: AccountRepository) {}
+
+	async verifyExists(id: string) {
+		const founId = await this.repository.findById(id);
+
+		if (!founId) {
+			throw new NotFoundException("id not found");
+		}
+
+		return founId;
+	}
 }
