febb92c413908d8d09a1d951d3967f7113759632
laxdbit <lennon_ximenes@hotmail.com>
2025-06-12 18:38:16 -0300

    feat: refactor project to apply Domain-Driven Design (DDD) structure

--- src/module/account/account.repository.ts
+++ src/module/account/account.repository.ts
@@ -5,33 +5,4 @@ import { CreateAccountDto } from "./dto/create.dto";
 @Injectable()
 export class AccountRepository {
   constructor(private readonly prisma: PrismaService) {}
-
-  async create(data: CreateAccountDto): Promise<any> {
-    const { user_id, ...rest } = data;
-    return await this.prisma.account.create({
-      data: {
-        ...rest,
-        User: {
-          connect: {
-            id: user_id,
-          },
-        },
-      },
-    });
-  }
-
-  async findById(id: string): Promise<any> {
-    return await this.prisma.account.findUnique({
-      where: { user_id: id },
-    });
-  }
-
-  async getNextCode(): Promise<number> {
-    const lastAccount = await this.prisma.account.findFirst({
-      orderBy: { code: "desc" },
-      select: { code: true },
-    });
-
-    return lastAccount ? Number(lastAccount.code) + 1 : 1;
-  }
 }
